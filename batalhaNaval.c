19d49e6b4de51f49aa7c12ebb15b6832d017c7a3
Programacaounisuam <fgomesf96@gmail.com>
2025-03-08 17:37:44 +0000

    Desafio da batalha batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,99 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define linha 11
+#define coluna 11
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main (){
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //Variável da matriz de linha e coluna com valor 0
+    int matriz [linha][coluna] = {0};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //Impressão dos návios em linha e coluna
+    matriz [2][4] =3 ;
+    matriz [2][5] =3 ;
+    matriz [2][6] =3 ;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    matriz [5][8] =3 ;
+    matriz [6][8] =3 ;
+    matriz [7][8] =3 ;
+
+
+    //Impressão do rodapé de A à J
+    char linha1 [11] = {'X','A','B','C','D','E','F','G','H','I','J'};
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    for (int i = 0; i < 11; i++)
+    {
+        printf("%2c" , linha1[i]);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    }
+    printf("\n");
 
+    //Impressão da coluna de 1 à 10
+    for (int i = 0; i < 10; i++)
+    {
+        matriz[i][0] = i + 1 ;
+    }
+    
+    //Lógica para imprimir os návios em diagonal
+    for (int i = 4; i < 7; i++)
+    {
+        for (int j = 8; j > 5; j--)
+        {
+            matriz[j][9 - j] = 3;
+        }
+        matriz [i][i] = 3;
+    }
+
+
+    //Lógica da cruz
+
+    for (int i = 5; i < 10; i++)
+    {
+        for (int j = 5; j < 10; j++)
+        { 
+            if (i == 6 && ( j == 6 || j == 7 || j == 8 || j==9)){
+                matriz [i][j] = 5;
+            }
+            else if (i ==7 && (j == 6 || j == 7 || j == 8 || j == 9 )){
+                matriz[i] [j] = 5;
+            }
+            else if (j ==7 && (i == 5 || i == 6 || i == 7 || i == 8 )){
+                matriz[i] [j] = 5;}
+            else if (j ==8 && (i == 5 || i == 6 || i == 7 || i == 8 )){
+                matriz[i] [j] = 5;}  
+        
+         }
+                         
+        }
+
+
+    //Lógica da impressão do cone
+    for (int i = 2; i < 5; i++)
+    {
+        for (int j = 1; j < 6; j++)
+        { 
+         if (i == 2 && j == 3){
+            matriz[i][j] = 5;
+        }
+            else if (i == 3 && ( j == 2 || j == 3 || j == 4)){
+                matriz [i][j] = 5;
+            }
+            else if (i ==4 && (j == 1 || j == 2 || j == 3 || j == 4 || j == 5 )){
+                matriz[i] [j] = 5;
+            }
+         }
+                         
+        }
+    
+    //Lógica para impressão do tabuleito
+    for (int i = 0; i < 10 ; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            printf("%2d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
